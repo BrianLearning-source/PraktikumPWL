090fca84ce684fd42d81f17ad4f81625d4070ada
Brian Serafino Donovan <briandonovan696@gmail.com>
2026-03-30 06:08:31 +0700

    Custom Layout Form with Section

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -12,31 +12,264 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicon;
+use Filament\Schemas\Components\Group;
 
 class PostForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
-            ->components([
-                //
-                TextInput::make("title")
-                ->minLength(5),
-                TextInput::make("slug")
-                ->unique(ignoreRecord: true),
-                Select::make('category_id')
-                    ->relationship('category', 'name')
-                    ->preload()
-                    ->searchable(),
-                ColorPicker::make('color'),
-                MarkdownEditor::make('content'),
-                //RichEditor::make('content'),
-                FileUpload::make('image')
-                ->disk('public')
-                ->directory('posts'),
-                TagsInput::make('tags'),
-                Checkbox::make('published'),
-                DateTimePicker::make('published_at'),
-            ]);
+
+->components([
+            //section 1 - post details
+            Section::make("Post Details")
+            -> description("Fill in the details of the post")
+            // -> icon(Heroicon::Rockerlaunch)
+            -> icon('heroicon-o-document-text')
+            ->schema([
+                //grouping fiels into 2 colums
+                Group::make([
+                TextInput::make("title"),
+                Textinput::make("slug"),
+                Select::make("category_id")
+                ->relationship("category", "name")
+                ->preload()
+                ->searchable(),
+                ColorPicker::make("color"),
+                ])->columns(2),
+                
+                MarkdownEditor::make("content"),
+            ])->columnSpan(2),
+
+            //Grouping fields into 2 columns
+            Group::make([
+
+                // section 2 - Image
+                Section::make("Image upload")
+                ->icon('heroicon-o-photo')
+                ->schema([
+                    FileUpload::make("image")
+                    ->disk("public")
+                    ->directory("posts"),
+                ]),
+            
+
+            //section 3 - meta
+                Section::make("Meta Information")
+                ->icon('heroicon-o-archive-box')
+                ->schema([
+                    TagsInput::make("tags"),
+                    Checkbox::make("published"),
+                    DateTimePicker::make("published_at"),
+                ]),
+                ])->columnSpan(1),
+            ])->columns(3);
+
+
+        
+// ->components([
+//             //section 1 - post details
+//             Section::make("Post Details")
+//             -> description("Fill in the details of the post")
+//             // -> icon(Heroicon::Rockerlaunch)
+//             -> icon('heroicon-o-document-text')
+//             ->schema([
+//                 //grouping fiels into 2 colums
+//                 Group::make([
+//                 TextInput::make("title"),
+//                 Textinput::make("slug"),
+//                 Select::make("category_id")
+//                 ->relationship("category", "name")
+//                 ->preload()
+//                 ->searchable(),
+//                 ColorPicker::make("color"),
+//                 ])->columns(2),
+                
+//                 MarkdownEditor::make("content"),
+//             ])->columnSpan(2),
+
+//             //Grouping fields into 2 columns
+//             Group::make([
+
+//                 // section 2 - Image
+//                 Section::make("Image upload")
+//                 ->schema([
+//                     FileUpload::make("image")
+//                     ->disk("public")
+//                     ->directory("posts"),
+//                 ]),
+            
+
+//             //section 3 - meta
+//                 Section::make("Meta Information")
+//                 ->schema([
+//                     TagsInput::make("tags"),
+//                     Checkbox::make("published"),
+//                     DateTimePicker::make("published_at"),
+//                 ]),
+//                 ])->columnSpan(1),
+//             ])->columns(3);
+
+            
+
+
+// ->components([
+//             //section 1 - post details
+//             Section::make("Post Details")
+//             -> description("Fill in the details of the post")
+//             // -> icon(Heroicon::Rockerlaunch)
+//             -> icon('heroicon-o-document-text')
+//             ->schema([
+//                 TextInput::make("title"),
+//                 Textinput::make("slug"),
+//                 Select::make("category_id")
+//                 ->relationship("category", "name")
+//                 ->preload()
+//                 ->searchable(),
+//                 ColorPicker::make("color"),
+//                 MarkdownEditor::make("content"),
+//             ])->columnSpan(2),
+
+//             //Grouping fields into 2 columns
+//             Group::make([
+
+//                 // section 2 - Image
+//                 Section::make("Image upload")
+//                 ->schema([
+//                     FileUpload::make("image")
+//                     ->disk("public")
+//                     ->directory("posts"),
+//                 ]),
+            
+
+//             //section 3 - meta
+//                 Section::make("Meta Information")
+//                 ->schema([
+//                     TagsInput::make("tags"),
+//                     Checkbox::make("published"),
+//                     DateTimePicker::make("published_at"),
+//                 ]),
+//                 ])->columnSpan(1),
+//             ])->columns(3);
+
+            // ->components([
+            // //section 1 - post details
+            // Section::make("Post Details")
+            // -> description("Fill in the details of the post")
+            // // -> icon(Heroicon::Rockerlaunch)
+            // -> icon('heroicon-o-document-text')
+            // ->schema([
+            //     TextInput::make("title"),
+            //     Textinput::make("slug"),
+            //     Select::make("category_id")
+            //     ->relationship("category", "name")
+            //     ->preload()
+            //     ->searchable(),
+            //     ColorPicker::make("color"),
+            //     MarkdownEditor::make("content"),
+            // ]),
+
+            // //Grouping fields into 2 columns
+            // Group::make([
+
+            //     // section 2 - Image
+            //     Section::make("Image upload")
+            //     ->schema([
+            //         FileUpload::make("image")
+            //         ->disk("public")
+            //         ->directory("posts"),
+            //     ]),
+            
+
+            // //section 3 - meta
+            //     Section::make("Meta Information")
+            //     ->schema([
+            //             TagsInput::make("tags"),
+            //             Checkbox::make("published"),
+            //     ])->columns(2),
+
+            //  DateTimePicker::make("published_at"),
+            // ]),
+            // ])->columns(2);
+
+            
+
+
+            // ->components([
+            // //section 1 - post details
+            // Section::make("Post Details")
+            // -> description("Fill in the details of the post")
+            // // -> icon(Heroicon::Rockerlaunch)
+            // -> icon('heroicon-o-document-text')
+            // ->schema([
+            //     TextInput::make("title"),
+            //     Textinput::make("slug"),
+            //     Select::make("category_id")
+            //     ->relationship("category", "name")
+            //     ->preload()
+            //     ->searchable(),
+            //     ColorPicker::make("color"),
+            //     MarkdownEditor::make("content"),
+            // ])->columnSpanFull(),
+
+            // // section 2 - Image
+            //     Section::make("Image upload")
+            //     ->schema([
+            //         FileUpload::make("image")
+            //         ->disk("public")
+            //         ->directory("posts"),
+            //     ]),
+
+            // //section 3 - meta
+            //     Section::make("Meta Information")
+            //     ->schema([
+            //             TagsInput::make("tags"),
+            //             Checkbox::make("published"),
+            //             DateTimePicker::make("published_at"),
+            //     ])
+
+            // ->components([
+            //     // section 1 - post details
+            //     Section::make("Post Details")
+            //     ->Description("Fill in the details of the posts")
+            //     // -> icon(Heroicon::RocketLaunch)
+            //     -> icon('heroicon-o-document-text')
+            //     ->schema([
+            //         TextInput::make("title"),
+            //         TextInput::make("slug"),
+            //         Select::make('category_id')
+            //         ->relationship('category', 'name')
+            //         ->preload()
+            //         ->searchable(),
+            //          ColorPicker::make('color'),
+            //         MarkdownEditor::make('content'),
+            //     ]),
+                
+            //     //section 2 - Image
+            //     Section::make("Image upload")
+            //     ->schema([
+            //         FileUpload::make("image")
+            //         ->disk("public")
+            //         ->directory("posts"),
+            //     ]),
+
+            //     //section 3 - meta
+            //     Section::make("Meta Information")
+            //     ->schema([
+            //             TagsInput::make("tags"),
+            //             Checkbox::make("published"),
+            //             DateTimePicker::make("published_at"),
+            //     ])
+                
+                // RichEditor::make('content'),
+                // FileUpload::make('image')
+                // ->disk('public')
+                // ->directory('posts'),
+                // TagsInput::make('tags'),
+                // Checkbox::make('published'),
+                // DateTimePicker::make('published_at'),
+            
     }
 }
